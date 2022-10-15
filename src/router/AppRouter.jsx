f6ad26f4706f457e58ef328754bf6449dac6f6fe
Cristhian Arostegui <cr.arostegui@gmail.com>
2022-10-15 11:59:19 -0400

    Add navbar, add barrel files for an easy import

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,15 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { LoginPage } from "../auth/pages/LoginPage"
-import { DcPage } from "../heroes/pages/DcPage"
-import { MarvelPage } from "../heroes/pages/MarvelPage"
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import { Navbar } from '../ui';
+
+import { LoginPage } from '../auth';
+import { MarvelPage, DcPage } from '../heroes';
 
 export const AppRouter = () => {
   return (
     <>
+        <Navbar />
+
         <Routes>
             <Route path="marvel" element={<MarvelPage />} />
             <Route path="dc" element={<DcPage />} />
