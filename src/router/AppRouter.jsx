d321396a424fdf05a7d01642db376830df2d16e3
Cristhian Arostegui <cr.arostegui@gmail.com>
2022-10-16 11:17:43 -0400

    Separate routes, create new HeroesRoutes files, add HeroPage and SearchPage components

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,20 +3,15 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { Navbar } from '../ui';
 
 import { LoginPage } from '../auth';
-import { MarvelPage, DcPage } from '../heroes';
+import { HeroesRoutes } from '../heroes';
 
 export const AppRouter = () => {
   return (
     <>
-        <Navbar />
-
         <Routes>
-            <Route path="marvel" element={<MarvelPage />} />
-            <Route path="dc" element={<DcPage />} />
-
             <Route path="login" element={<LoginPage />} />
 
-            <Route path="/" element={<Navigate to="/marvel" />} />
+            <Route path="/*" element={<HeroesRoutes />} />
         </Routes>
     </>
   )
